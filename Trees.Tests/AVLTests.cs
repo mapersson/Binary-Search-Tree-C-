44a82aeb3aa07f63ff5a498f4c40f69fcfa4552e
mapersson <62900241+mapersson@users.noreply.github.com>
2020-12-12 10:46:30 -0700

    Added Unit Testing
    Added MSTest framework and test classes.

---
+++ Trees.Tests/AVLTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Trees.Tests
+{
+    [TestClass]
+    public class AVLTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Trees.Tests/BSTTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Trees.Tests
+{
+    [TestClass]
+    public class BSTTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
